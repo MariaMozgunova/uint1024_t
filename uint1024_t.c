5f68fb3fe731ebc5ffa29c6e863c552558cc6420
Maria Mozgunova <bagira_2003@inbox.ru>
2021-12-17 23:58:33 +0300

    Add workflow

--- uint1024_t.c
+++ uint1024_t.c
@@ -209,43 +209,43 @@ uint1024_t mult_op(uint1024_t * x, uint1024_t * y) {
     return res;
 }
 
-// int main(int argc, char* argv[]) {
-//     uint1024_t x;
-//     uint1024_t y;
-//     uint1024_t res;
-//     printf("Type in the uint1024_t number: ");
-//     scanf_value(&x);
-//     printf("Here is the number you typed in: ");
-//     printf_value(&x);
-//     printf("\n\n");
+int main(int argc, char* argv[]) {
+    uint1024_t x;
+    uint1024_t y;
+    uint1024_t res;
+    printf("Type in the uint1024_t number: ");
+    scanf_value(&x);
+    printf("Here is the number you typed in: ");
+    printf_value(&x);
+    printf("\n\n");
 
-//     x = from_uint(56547216);  // CHANGEME
-//     y = from_uint(75855);  // CHANGEME
-//     printf_value(&x);
-//     printf(" + ");
-//     printf_value(&y);
-//     printf(" = ");
-//     res = add_op(&x, &y);
-//     printf_value(&res);
-//     printf("\n");
+    x = from_uint(56547216);  // CHANGEME
+    y = from_uint(75855);  // CHANGEME
+    printf_value(&x);
+    printf(" + ");
+    printf_value(&y);
+    printf(" = ");
+    res = add_op(&x, &y);
+    printf_value(&res);
+    printf("\n");
     
-//     printf_value(&x);
-//     printf(" - ");
-//     printf_value(&y);
-//     printf(" = ");
-//     free(res.num);
-//     res = subtr_op(&x, &y);
-//     printf_value(&res);
-//     printf("\n");
+    printf_value(&x);
+    printf(" - ");
+    printf_value(&y);
+    printf(" = ");
+    free(res.num);
+    res = subtr_op(&x, &y);
+    printf_value(&res);
+    printf("\n");
     
-//     printf_value(&x);
-//     printf(" * ");
-//     printf_value(&y);
-//     printf(" = ");
-//     free(res.num);
-//     res = mult_op(&x, &y);
-//     printf_value(&res);
-//     printf("\n");
+    printf_value(&x);
+    printf(" * ");
+    printf_value(&y);
+    printf(" = ");
+    free(res.num);
+    res = mult_op(&x, &y);
+    printf_value(&res);
+    printf("\n");
 
-//     return 0;
-// }
+    return 0;
+}
